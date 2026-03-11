520ef728b6ab6b72eb5c1f7b3a6080554e681dfb
shivaganesh720 <naguruganesh2005@gmail.com>
2026-03-11 14:23:02 +0530

    updated logout.jsx

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
 function Logout() {
     return (
         <div>
             <h2>You have been logged out</h2>
+            <p><Link to="/login">Login again</Link></p>
         </div>
     );
 }
